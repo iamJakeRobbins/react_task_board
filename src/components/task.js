ad5a043d4d9507abda313c64ae25ed3ff27b3ba2
Jake Robbins <jrobbinsd@gmail.com>
2019-08-29 19:05:28 -0600

    sample task component

---
+++ src/components/task.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+const SampleTask = () => {
+	let template =
+	<div class="new_task">
+		<span> this will be a task </span>
+	</div>
+	return template
+};
+
+export default SampleTask;
