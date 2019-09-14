1532b722fd05b7e01d50358c01d92b327e4097c2
Jake Robbins <jrobbinsd@gmail.com>
2019-09-14 17:21:12 -0600

    import sidebar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import { BrowserRouter, Route, Switch } from "react-router-dom"
-import Column from './components/Column'
-import CreateNew from './components/CreateNew'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import Login from './components/Login'
-import Task from './components/Task'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Column from './components/Column';
+import CreateNew from './components/CreateNew';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Login from './components/Login';
+import Task from './components/Task';
+import SideMenu from './components/SideMenu';
 
 
 class App extends Component {
