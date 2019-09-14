e2e2c4a974155fc162ef8c1357ce8ca430e3d3ba
Jake Robbins <jrobbinsd@gmail.com>
2019-09-14 17:21:35 -0600

    define new sidebar component

---
+++ src/components/SideMenu.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SideMenu extends Component{
+  render(){
+    return(
+      <div id="sideMenu">
+			
+			</div>
+    );
+  }
+}
+export default SideMenu;
