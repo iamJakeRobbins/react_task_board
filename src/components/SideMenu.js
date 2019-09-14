52e093ff5f943591225bac42003b71a082224fbf
Jake Robbins <jrobbinsd@gmail.com>
2019-09-14 17:55:35 -0600

    sidebar

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
 
 class SideMenu extends Component{
+	constructor(props, context) {
+  super(props, context);
+
+  this.state = {
+    visible: false
+  };
+
+  this.toggleMenu = this.toggleMenu.bind(this);
+	this.handleMouseDown = this.handleMouseDown.bind(this);
+}
+
+handleMouseDown(e) {
+	this.toggleMenu();
+
+	console.log("clicked");
+	e.stopPropagation();
+}
+
+toggleMenu() {
+  this.setState({
+      visible: !this.state.visible
+  });
+	console.log(this.state)
+}
+
   render(){
+		var visibility = "hide";
+
+	 if (this.state.visible) {
+		 visibility = "show";
+	 }
     return(
-      <div id="sideMenu">
-			
+      <div id="sideMenu" className={visibility}>
+			<div id="controlSideMenu">
+				<button onMouseDown={this.handleMouseDown}>boop</button>
+			</div>
+				<ul>
+					<li>My HomePage</li>
+					<li>Add Column</li>
+					<li>Add Button</li>
+				</ul>
 			</div>
     );
   }
