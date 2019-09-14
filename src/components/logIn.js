498e02e22dcbb3e50f4ebfe044b86a8607800972
Jake Robbins <jrobbinsd@gmail.com>
2019-09-14 17:21:56 -0600

    add form fields for user input

--- src/components/logIn.js
+++ src/components/logIn.js
@@ -5,6 +5,12 @@ class Login extends Component{
     return(
       <div>
         <form>
+					<div>
+						<input type="text" id="userName" placeholder="Enter User Name"></input>
+					</div>
+					<div>
+						<input type="text" id="password" placeholder="Enter Password"></input>
+					</div>
           <button>
             Submit
           </button>
